83ce54f456b08ea30bed125cd44c2ca05629b283
Stino01 <agostino.carrieri01@gmail.com>
2022-04-22 16:41:26 +0200

    add print array and add/remove element in list

--- script.js
+++ script.js
@@ -1,9 +1,36 @@
 const app = new Vue({
     el: '#app',
     data: {
-
+        oggetti: [
+            {
+                text: 'Comprare il latte',
+                done: false,
+            },
+            {
+                text: 'Stendere la lavatrice',
+                done: false,
+            },
+            {
+                text: 'Pulire la camera',
+                done: false,
+            }
+        ],
+        nuovoObj: ''
     },
     methods: {
-        
+        remove(i){
+            this.oggetti.splice(i, 1)
+        },
+        add(){
+            const newObj = {
+                text: this.nuovoObj, 
+                check: true,
+            }
+            this.oggetti.push(newObj)
+        }
     }
 })
+
+//Stampare all'interno di una lista, un item per ogni todo e se la proprietà done è uguale a true, visualizzare il testo del todo sbarrato.
+
+//BONUS 2- cliccando sul testo dell'item, invertire il valore della proprietà done del todo corrispondente (se done era uguale a false, impostare true e viceversa)
